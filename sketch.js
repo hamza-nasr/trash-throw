8c08ff93deab02ec613a9a1ee4928bdfe034cee8
hamza-nasr <68786227+hamza-nasr@users.noreply.github.com>
2020-09-26 15:09:26 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+	
+	
+	box1=Bodies.rectangle(650,height-50,200,20,{isStatic:true});
+	
+	box2=Bodies.rectangle(550,height-100,20,100,{isStatic:true});
+	
+	box3=Bodies.rectangle(750,height-100,20,100,{isStatic:true});
+	
+	paper=Bodies.circle(50,600,30,{restitution:0.8});
+	
+	ground=Bodies.rectangle(400,height,800,20,{isStatic:true})
+	
+	//Create the Bodies Here.
+	World.add(world,box1);
+	World.add(world,box2);
+	World.add(world,box3);
+	World.add(world,paper);
+	World.add(world,ground);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  Engine.update(engine)
+  background(0);
+  rect(box1.position.x,box1.position.y,200,20);
+  rect(box2.position.x,box2.position.y,20,100);
+  rect(box3.position.x,box3.position.y,20,100);
+  rect(ground.position.x,ground.position.y,800,20)
+  ellipseMode(RADIUS);
+  ellipse(paper.position.x,paper.position.y,30,30);
+}
+function keyPressed() {
+	if(keyCode === UP_ARROW){
+	 Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+	}
+}
+
+
